c872c400e5c8a501734d3bab61b76502a373b2d1
Shai gold <shaigold24@gmail.com>
2024-01-01 11:33:17 +0200

    multi client support

--- source/repos/server/server.cs
+++ source/repos/server/server.cs
@@ -5,43 +5,49 @@ using System.Text;
 
 class TCPServer
 {
-    static void Main()
+    static async Task Main()
     {
         IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
         var port = 8080;
         TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+        Console.WriteLine("Listening");
+
         while (true)
         {
-            listener.Start();
-            Console.WriteLine("Listening");
-            TcpClient client = listener.AcceptTcpClient();
+            TcpClient client = await listener.AcceptTcpClientAsync();
             Console.WriteLine("Client connected");
-
-            var recvMsg = "";
-            NetworkStream stream = client.GetStream();
-            var sendMsg = "got it";
-            var writeBuffer = Encoding.ASCII.GetBytes(sendMsg);
-
-            while (recvMsg != "Bye")
-            {
-
-                var readBuffer = new byte[1024];
-                var bytesRead = stream.Read(readBuffer, 0, readBuffer.Length);
-                recvMsg = Encoding.ASCII.GetString(readBuffer, 0, bytesRead);
-                Console.WriteLine("the client sent: " + recvMsg);
-                if (recvMsg != "Bye")
-                {
-                    stream.Write(writeBuffer, 0, writeBuffer.Length);
-                }
-
-            }
-
-            sendMsg = "done";
-            writeBuffer = Encoding.ASCII.GetBytes(sendMsg);
-            stream.Write(writeBuffer, 0, writeBuffer.Length);
-
-            Console.WriteLine("closing the connection");
-            client.Close();
+            _= HandleClient(client);
         }
     }
-}
+
+
+    static async Task HandleClient(TcpClient client) { 
+
+        var recvMsg = "";
+        NetworkStream stream = client.GetStream();
+        var sendMsg = "got it";
+        var writeBuffer = Encoding.ASCII.GetBytes(sendMsg);
+
+        while (recvMsg != "Bye")
+        {
+
+            var readBuffer = new byte[1024];
+            var bytesRead = await stream.ReadAsync(readBuffer, 0, readBuffer.Length);
+            recvMsg = Encoding.ASCII.GetString(readBuffer, 0, bytesRead);
+            Console.WriteLine("the client sent: " + recvMsg);
+            if (recvMsg != "Bye")
+            {
+                await stream.WriteAsync(writeBuffer, 0, writeBuffer.Length);
+            }
+
+        }
+
+        sendMsg = "done";
+        writeBuffer = Encoding.ASCII.GetBytes(sendMsg);
+        await stream.WriteAsync(writeBuffer, 0, writeBuffer.Length);
+
+        Console.WriteLine("closing the connection");
+        client.Close();
+        }
+    }
